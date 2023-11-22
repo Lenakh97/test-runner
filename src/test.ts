017a5151b4b7480aacfc60deb2c741ea9df177a9
lenakh97 <lenaharaldseid@gmail.com>
2023-11-22 14:36:47 +0100

    feat: add support for async functions

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,29 @@
-export const test = (description: string, callback: Function): void => {
+export const test = (
+  description: string,
+  assertionContext: () => void | Promise<void>
+) => {
+  try {
+    isPromise(assertionContext())
+      ? callAsync(
+          description,
+          assertionContext as unknown as () => Promise<void>
+        )
+      : call(description, assertionContext);
+  } catch (error) {
+    console.log(`❌ ${description}`);
+  }
+};
+
+const isPromise = (maybePromise: void | Promise<void>) =>
+  !!maybePromise &&
+  typeof (maybePromise as unknown as Promise<unknown>).then === "function";
+
+const callAsync = (description: string, callback: () => Promise<void>) =>
+  callback()
+    .then(() => console.log(`✅ ${description}`))
+    .catch((error) => console.log(`❌ ${description}`));
+
+const call = (description: string, callback: () => void) => {
   callback();
+  console.log(`✅ ${description}`);
 };
