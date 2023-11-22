09fde8bcd3ef80a2835edfa595e04e67661fd96b
lenakh97 <lenaharaldseid@gmail.com>
2023-11-22 14:37:05 +0100

    fix: add test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,27 @@
 import { describe } from "./describe.js";
 import { expect2 } from "./assertion.js";
 import { test as it } from "./test.js";
+import {
+  asyncMultiplication,
+  asyncSubtract,
+  multiplication,
+  subtract,
+} from "./index.js";
 
 describe("This test will check an assertion", () => {
   it("should check if two strings are equal", () => {
     expect2("hei").toBe("hei");
   });
-  it("should let the user know if two string are not equal", () => {
-    expect2("hei").toBe("hei2");
+  it("should subtract numbers in an array", () => {
+    expect2(subtract([5, 3, 1])).toBe(-9);
+  });
+  it("should subtract numbers in an array (async)", async () => {
+    expect2(await asyncSubtract([5, 3, 1])).toBe(-9);
+  });
+  it("should multiply two numbers", () => {
+    expect2(multiplication(5, 3)).toBe(15);
+  });
+  it("should multiply two numbers", async () => {
+    expect2(await asyncMultiplication(5, 3)).toBe(15);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+export const subtract = (numbers: number[]): number =>
+  numbers.reduce((a, b) => a - b, 0);
 
+export const asyncSubtract = (numbers: number[]): Promise<number> =>
+  Promise.resolve(subtract(numbers));
+
+export const multiplication = (a: number, b: number): number => a * b;
+
+export const asyncMultiplication = (a: number, b: number): Promise<number> =>
+  Promise.resolve(multiplication(a, b));
